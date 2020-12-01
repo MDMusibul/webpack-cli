074b9ab78c0d4bd298d77ad48d45af66a2b36b34
Nitin Kumar <snitin315@gmail.com>
2020-12-01 17:39:20 +0530

    tests: exit-code

--- test/merge/config-absent/merge-config-absent.test.js
+++ test/merge/config-absent/merge-config-absent.test.js
@@ -8,8 +8,9 @@ const { run } = require('../../utils/test-utils');
 describe('merge flag configuration', () => {
     it('Show warning message when the merge config is absent', () => {
         // 2.js doesn't exist, let's try merging with it
-        const { stdout, stderr } = run(__dirname, ['--config', './1.js', '--config', './2.js', '--merge'], false);
+        const { exitCode, stderr, stdout } = run(__dirname, ['--config', './1.js', '--config', './2.js', '--merge'], false);
 
+        expect(exitCode).toEqual(2);
         // Since the process will exit, nothing on stdout
         expect(stdout).toBeFalsy();
         // Confirm that the user is notified
